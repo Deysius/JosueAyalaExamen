258c79db44483f2cb667b848ba383423b698c11a
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2025-04-28 08:23:45 -0500

    Clases implementadas

---
+++ JosueAyalaExamen/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JosueAyalaExamen.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Range(18, 100)]
+        public int Edad { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Saldo { get; set; }
+
+        [Display]
+        public bool Miembro { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro { get; set; }
+
+    }
+}
---
+++ JosueAyalaExamen/Models/Recompensa.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JosueAyalaExamen.Models
+{
+    public class Recompensa
+    {
+        [Key]
+        public int RecompensaId { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaInicio { get; set; }
+
+        public int PuntosAcumulados { get; set; }
+
+        public string TipoRecompensa
+        {
+            get
+            {
+                return PuntosAcumulados < 500 ? "SILVER" : "GOLD";
+            }
+        }
+    }
+}

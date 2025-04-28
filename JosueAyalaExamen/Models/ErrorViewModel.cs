0bad25c1f479646a710a65b88d390486302174ef
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2025-04-28 08:22:16 -0500

    Agregar archivos de proyecto.

---
+++ JosueAyalaExamen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JosueAyalaExamen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

87b8179c789d8b70bd377df39d0805825adb4a80
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2025-04-28 08:24:33 -0500

    Nueva clase reserva

---
+++ JosueAyalaExamen/Models/Reserva.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JosueAyalaExamen.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int ReservaId { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime FechaEntrada { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime FechaSalida { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
+        public decimal ValorPagar { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
